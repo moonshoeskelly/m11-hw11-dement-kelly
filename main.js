5209c4f92c43f9ac25c00236806ecf9e7a0f294c
Kelly DeMent <kdement@ufl.edu>
2019-11-01 20:23:20 -0400

    Add event listener to run function on click

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ function loopThrough () {
   for (let i=0; i < this.textContent.length * 2; i++) {
     setTimeout (changeColor, i * 1000);
   };
-  
+
 };
 
 //The Event Listener
+for (let i=0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
