30bec43df9bf85cbff281ab78e9a37edd5412d24
Kelly DeMent <kdement@ufl.edu>
2019-11-01 20:21:55 -0400

    Add toogle functionality to change color

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
+function loopThrough () {
+  const changeColor = () => this.classList.toggle('red');
 
+  for (let i=0; i < this.textContent.length * 2; i++) {
+    setTimeout (changeColor, i * 1000);
+  };
+  
+};
 
 //The Event Listener
